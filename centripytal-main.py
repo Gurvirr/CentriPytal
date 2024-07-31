e011e1bd872a110f1f134b5276b2bcf3977d0e25
Gurvir <gurvirrandhawa28@gmail.com>
2024-07-31 12:24:42 -0400

    Fixed attribute errors

--- centripytal-main.py
+++ centripytal-main.py
@@ -2,12 +2,12 @@ import pygetwindow as gw
 from pynput import keyboard
 
 def on_press(key):
-    if key == key.shift:
+    if key == keyboard.Key.shift:
         active_window = gw.getActiveWindow()
         active_window.minimize()
 
 def on_release(key):
-    if key == key.shift:
+    if key == keyboard.Key.shift:
         print("bye")
         
 with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
