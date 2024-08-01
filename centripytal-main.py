477aeb1a9b4d431d9c0f88a5f9d2d31f776c831e
Gurvir <gurvirrandhawa28@gmail.com>
2024-08-01 10:59:41 -0400

    Implemented window-centering functionality

--- centripytal-main.py
+++ centripytal-main.py
@@ -3,14 +3,15 @@ from pynput import keyboard
 from screeninfo import get_monitors
 
 for m in get_monitors():
-    width = int(m.width)
-    height = int(m.height)
+    monitor_width = m.width
+    monitor_height = m.height
 
 def on_press(key):
     if key == keyboard.Key.shift:
         active_window = gw.getActiveWindow()
-        print(active_window)
-        active_window.moveTo(width // 2, height // 2)
+        x = (monitor_width // 2) - (active_window.width // 2)
+        y = (monitor_height // 2) - (active_window.height // 2)
+        active_window.moveTo(x, y)
 
 def on_release(key):
     if key == keyboard.Key.shift:
