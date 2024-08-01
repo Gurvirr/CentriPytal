ff40b3bb5cd889d949abb308c1ac42543bf4accc
Gurvir <gurvirrandhawa28@gmail.com>
2024-08-01 10:58:36 -0400

    Added window moving functionality

--- centripytal-main.py
+++ centripytal-main.py
@@ -1,14 +1,20 @@
 import pygetwindow as gw
 from pynput import keyboard
+from screeninfo import get_monitors
+
+for m in get_monitors():
+    width = int(m.width)
+    height = int(m.height)
 
 def on_press(key):
     if key == keyboard.Key.shift:
         active_window = gw.getActiveWindow()
-        active_window.minimize()
+        print(active_window)
+        active_window.moveTo(width // 2, height // 2)
 
 def on_release(key):
     if key == keyboard.Key.shift:
         print("bye")
-        
+
 with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
