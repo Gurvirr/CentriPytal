22c8547361bc0a1c63d8fdca1a2ed1c6a41fa1c5
Gurvir <gurvirrandhawa28@gmail.com>
2024-08-01 11:11:50 -0400

    Optimized the code to be even shorter than before lol

--- centripytal-main.py
+++ centripytal-main.py
@@ -2,15 +2,12 @@ import pygetwindow as gw
 from pynput import keyboard
 from screeninfo import get_monitors
 
-for m in get_monitors():
-    monitor_width = m.width
-    monitor_height = m.height
-
 def on_press(key):
     if key == keyboard.Key.shift:
         active_window = gw.getActiveWindow()
-        x = (monitor_width // 2) - (active_window.width // 2)
-        y = (monitor_height // 2) - (active_window.height // 2)
+        monitor = get_monitors()[0]
+        x = (monitor.width // 2) - (active_window.width // 2)
+        y = (monitor.height // 2) - (active_window.height // 2)
         active_window.moveTo(x, y)
 
 with keyboard.Listener(on_press = on_press) as listener:
