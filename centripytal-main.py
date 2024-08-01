1a17a8a52af03bd3db8f0f34ffcfb0a32bcdc9e6
Gurvir <gurvirrandhawa28@gmail.com>
2024-08-01 11:00:21 -0400

    Removed unnecessary code

--- centripytal-main.py
+++ centripytal-main.py
@@ -13,9 +13,5 @@ def on_press(key):
         y = (monitor_height // 2) - (active_window.height // 2)
         active_window.moveTo(x, y)
 
-def on_release(key):
-    if key == keyboard.Key.shift:
-        print("bye")
-
-with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
+with keyboard.Listener(on_press = on_press) as listener:
     listener.join()
